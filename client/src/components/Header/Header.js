ad813a9a6db9e93b5685eeed5801b8d800f58b99
muhamamdzeeshan2 <muhammadzeeshannkn@gmail.com>
2024-01-24 15:25:27 +0500

    initial commit

---
+++ client/src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import 'src/components/Header/Header.css';
+
+function Header() {
+    return (
+        <div className='Header-wrapper'>
+            <h1>Projectr</h1>
+        </div>
+    )
+}
+
+export default Header;
